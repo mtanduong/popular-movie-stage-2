be1a294f96294c614cbe86d6bde20118fb56c735
mtanduong <mtanduong@gmail.com>
2019-05-17 23:36:54 -0700

    Implemented MovieRepository

--- app/src/main/java/com/example/popularmoviesstage1/database/MovieRepository.java
+++ app/src/main/java/com/example/popularmoviesstage1/database/MovieRepository.java
@@ -1,4 +1,80 @@
 package com.example.popularmoviesstage1.database;
 
+import android.app.Application;
+import android.os.AsyncTask;
+
+import com.example.popularmoviesstage1.models.Movie;
+
+import java.util.List;
+
+import androidx.lifecycle.LiveData;
+
 public class MovieRepository {
+    private MovieDao movieDao;
+    private LiveData<List<Movie>> allMovies;
+
+    public MovieRepository(Application application) {
+        MovieDatabase database = MovieDatabase.getInstance(application);
+        movieDao = database.movieDao();
+        allMovies = movieDao.getAllMovies();
+    }
+
+    public void insert(Movie movie) {
+        new InsertMovieAsyncTask(movieDao).execute(movie);
+    }
+
+    public void update(Movie movie) {
+        new UpdateMovieAsyncTask(movieDao).execute(movie);
+    }
+
+    public void delete(Movie movie) {
+        new DeleteMovieAsyncTask(movieDao).execute(movie);
+    }
+
+    public LiveData<List<Movie>> getAllMovies() {
+        return allMovies;
+    }
+
+    private static class InsertMovieAsyncTask extends AsyncTask<Movie, Void, Void> {
+        private MovieDao movieDao;
+
+        private InsertMovieAsyncTask(MovieDao movieDao) {
+            this.movieDao = movieDao;
+        }
+
+        @Override
+        protected Void doInBackground(Movie... movies) {
+            movieDao.insert(movies[0]);
+            return null;
+        }
+    }
+
+    private static class UpdateMovieAsyncTask extends AsyncTask<Movie, Void, Void> {
+        private MovieDao movieDao;
+
+        private UpdateMovieAsyncTask(MovieDao movieDao) {
+            this.movieDao = movieDao;
+        }
+
+        @Override
+        protected Void doInBackground(Movie... movies) {
+            movieDao.update(movies[0]);
+            return null;
+        }
+    }
+
+    private static class DeleteMovieAsyncTask extends AsyncTask<Movie, Void, Void> {
+        private MovieDao movieDao;
+
+        private DeleteMovieAsyncTask(MovieDao movieDao) {
+            this.movieDao = movieDao;
+        }
+
+        @Override
+        protected Void doInBackground(Movie... movies) {
+            movieDao.delete(movies[0]);
+            return null;
+        }
+    }
+
 }
