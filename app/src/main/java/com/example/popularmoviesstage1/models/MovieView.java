bae94e58d1f3ef670b70cecb702e09152f175ea8
mtanduong <mtanduong@gmail.com>
2019-05-18 00:43:59 -0700

    Implemented MovieView

---
+++ app/src/main/java/com/example/popularmoviesstage1/models/MovieView.java
@@ -0,0 +1,39 @@
+package com.example.popularmoviesstage1.models;
+
+import android.app.Application;
+
+import com.example.popularmoviesstage1.database.MovieRepository;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+public class MovieView extends AndroidViewModel {
+
+    private MovieRepository repository;
+    private LiveData<List<Movie>> allMovies;
+
+    public MovieView(@NonNull Application application) {
+        super(application);
+        repository = new MovieRepository(application);
+        allMovies = repository.getAllMovies();
+    }
+
+    public void insert(Movie movie) {
+        repository.insert(movie);
+    }
+
+    public void update(Movie movie) {
+        repository.update(movie);
+    }
+
+    public void delete(Movie movie) {
+        repository.delete(movie);
+    }
+
+    public LiveData<List<Movie>> getAllMovies() {
+        return allMovies;
+    }
+}
