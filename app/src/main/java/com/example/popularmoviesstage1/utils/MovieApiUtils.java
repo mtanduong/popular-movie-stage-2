2c473f514ce48fdc63563cb6b210ba815685dc42
mtanduong <mtanduong@gmail.com>
2019-05-23 01:11:26 -0700

    Removed API key for security

--- app/src/main/java/com/example/popularmoviesstage1/utils/MovieApiUtils.java
+++ app/src/main/java/com/example/popularmoviesstage1/utils/MovieApiUtils.java
@@ -2,7 +2,7 @@ package com.example.popularmoviesstage1.utils;
 
 public class MovieApiUtils {
     // TODO - Enter API Key here
-    public static final String API_KEY = "e51e568877142d0363099bd88c03ded5";
+    public static final String API_KEY = "";
     private static final String BASE_URL = "https://api.themoviedb.org/3/";
     static final String POPULAR_PATH = "movie/popular";
     static final String TOP_RATED_PATH = "movie/top_rated";
